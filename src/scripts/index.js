2bb802631831d734a1be100b92ae6c71112c57ea
MartinChandra <88715733+Redevils03@users.noreply.github.com>
2023-11-14 17:36:01 +0700

    Update index.js

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -45,13 +45,37 @@ window.addEventListener('hashchange', () => {
   app.renderPage();
 });
 
+function generateFingerprint() {
+    // Use a Promise to handle the asynchronous loading of the library
+    return new Promise((resolve, reject) => {
+      // Create a script element
+      const script = document.createElement('script');
+
+      // Set the source to the FingerprintJS library
+      script.src = 'https://openfpcdn.io/fingerprintjs/v4';
+
+      // Set the onload event to resolve the Promise
+      script.onload = () => {
+        // Load the library and resolve the Promise with the FingerprintJS object
+        resolve(window.FingerprintJS.load());
+      };
+
+      // Set the onerror event to reject the Promise if the script fails to load
+      script.onerror = () => {
+        reject(new Error('Failed to load FingerprintJS'));
+      };
+
+      // Append the script to the document body
+      document.body.appendChild(script);
+    });
+  }
+
 window.addEventListener('load', () => {
   app.renderPage();
-  const fpPromise = import('https://openfpcdn.io/fingerprintjs/v4').then(FingerprintJS => FingerprintJS.load());
-
-  fpPromise
-      .then(fp => fp.get())
-      .then(result => alert(result.visitorId));
+  generateFingerprint()
+    .then(fp => fp.get())
+    .then(result => alert(result.visitorId))
+    .catch(error => console.error(error));
   swRegister();
 });
 
